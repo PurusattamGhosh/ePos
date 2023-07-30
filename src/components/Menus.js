1abdb07ad53a91e713f700cec1209d8228a02200
PurusattamGhosh <rka99puru@gmail.com>
2023-07-30 11:28:40 +0530

    modified

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -4,21 +4,21 @@ const Menus = {
           id: "b1",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b2",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b3",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         // Add more items up to b24
@@ -26,147 +26,147 @@ const Menus = {
           id: "b4",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b5",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b6",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b7",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b8",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b9",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b10",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b11",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b12",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b13",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b14",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b15",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b16",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b17",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b18",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b19",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b20",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b21",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b22",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b23",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         },
         {
           id: "b24",
           name: 'Cheese Burger',
           desc: 'Bun, Meat pattie, Cheese',
-          price: "RS 100",
+          price: 100,
           url: require("../../assets/images/hamburger.png")
         }
       ]
