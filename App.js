e336935f5b14766a230f24c58d6e74dc04a09796
PurusattamGhosh <rka99puru@gmail.com>
2023-08-02 22:53:05 +0530

    axios

--- App.js
+++ App.js
@@ -6,8 +6,24 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import product from './src/screens/product';
 import Home from './src/screens/Home';
 import LogIn from './src/screens/LogIn';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 export default function App() {
   const Stack = createNativeStackNavigator()
+  const [ipInfo, setIpInfo] = useState(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://ipinfo.io?token=5fa6bad942cd55'); 
+        setIpInfo(response.data); 
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  console.log(ipInfo)
   return (
     <PaperProvider theme={"light"}>
       
